13302241fe76e6d97f79a253b657a4e0c69102f5
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-05-20 16:59:14 -0400

    netlify form using react states

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,30 @@ import React from "react";
 import VanillaTilt from "vanilla-tilt";
 // import contactImg from "../assets/contact.png";
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 class Contact extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { name: "", email: "", message: "" };
   }
-  // handleSubmit = (e) => {
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({ "form-name": "contact", ...this.state }),
-  //   })
-  //     .then(() => alert("Success!"))
-  //     .catch((error) => alert(error));
+  handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
 
-  //   e.preventDefault();
-  // };
-  // handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+    e.preventDefault();
+  };
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   componentDidMount() {
     VanillaTilt.init(document.querySelectorAll(".main-content"), {
@@ -28,32 +34,48 @@ class Contact extends React.Component {
     });
   }
   render() {
+    const { name, email, message } = this.state;
     return (
       <div class="try">
         <div id="main-content" className="appear">
           <div className="main-content data-tilt">
-            <form name="contact" method="post">
-              <input type="hidden" name="form-name" value="contact" />
+            <form onSubmit={this.handleSubmit}>
               <p>
                 <label>
-                  Your Name: <input type="text" name="name" />
+                  Your Name:{" "}
+                  <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <label>
-                  Your Email: <input type="email" name="email" />
+                  Your Email:{" "}
+                  <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <label>
-                  Message: <textarea name="message"></textarea>
+                  Message:{" "}
+                  <textarea
+                    name="message"
+                    value={message}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <button type="submit">Send</button>
               </p>
             </form>
-            ,
           </div>
         </div>
       </div>
