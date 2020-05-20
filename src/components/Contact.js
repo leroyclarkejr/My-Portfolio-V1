7ae018ee6f8087ad38a86a6f787e89ada46ae6c6
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-05-20 16:42:48 -0400

    testing netlify form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,64 @@
 import React from "react";
+import VanillaTilt from "vanilla-tilt";
 // import contactImg from "../assets/contact.png";
 
-const Contact = () => (
-  <div id="contact">
-    <div className="main-contact">
-      <div class="form-container">
-        <h1>Get In Touch</h1>
-        <form>
-          <input type="text" name="name" placeholder="Name" height="40px" />
-          <br></br>
-          <input type="email" name="email" placeholder="Email address" />
-          <br></br>
-          <textarea
-            type="text"
-            className="message"
-            name="message"
-            placeholder="Message..."
-            rows={1}
-          />
-          <br></br>
-        </form>
-        <input className="submit" type="submit" value="Submit" />
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: "", email: "", message: "" };
+  }
+  // handleSubmit = (e) => {
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact", ...this.state }),
+  //   })
+  //     .then(() => alert("Success!"))
+  //     .catch((error) => alert(error));
+
+  //   e.preventDefault();
+  // };
+  // handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  componentDidMount() {
+    VanillaTilt.init(document.querySelectorAll(".main-content"), {
+      max: 25,
+      speed: 400,
+    });
+  }
+  render() {
+    return (
+      <div class="try">
+        <div id="main-content" className="appear">
+          <div className="main-content data-tilt">
+            <form name="contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message"></textarea>
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+            ,
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Contact;
